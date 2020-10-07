00819283914ad3a09a80eaf06bdf0fd716820141
Kapulkin Stanislav <kapulkin@gmail.com>
2020-10-07 21:23:15 +0300

    train draft

---
+++ train.py
@@ -0,0 +1,14 @@
+import segmentation_models as sm
+
+def main():
+    #TODO:
+    # load coco dataset: images, mask
+    # init model
+    # train
+    # iterate over dataset with batches
+    # 
+
+    pass
+
+if __name__ == '__main__':
+    main()
