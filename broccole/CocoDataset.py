5e24e8eace1738ca555d29331c824f6cdc00d481
Kapulkin Stanislav <kapulkin@gmail.com>
2020-10-08 05:38:17 +0300

    [+] train U-Net

--- broccole/CocoDataset.py
+++ broccole/CocoDataset.py
@@ -3,35 +3,45 @@ import cv2
 import os
 import random
 import numpy as np
+import tensorflow as tf
 from pycocotools import mask as cocoMask
 
 class CocoDataset:
-    def __init__(self, annotationFilePath: str, imagesDir: str, masksDir: str, shuffle: bool = True):
+    def __init__(self, annotationFilePath: str, imagesDir: str, masksDir: str, classes = [], shuffle: bool = True):
         with open(annotationFilePath, 'r') as annotationFile:
             annotations = json.load(annotationFile)
 
-        self.images = annotations["images"]
-
+        self.classes = { cls: cls for cls in classes } if len(classes) > 0 else None
         self.annotations = {}
         for annotation in annotations["annotations"]:
-            id = annotation["image_id"]
-            if id in self.annotations:
-                imageAnnotations = self.annotations[id]
-            else:
-                imageAnnotations = []
-                self.annotations[id] = imageAnnotations
-            imageAnnotations.append(annotation)
+            if self.classes is not None and annotation["category_id"] in self.classes:
+                id = annotation["image_id"]
+                if id in self.annotations:
+                    imageAnnotations = self.annotations[id]
+                else:
+                    imageAnnotations = []
+                    self.annotations[id] = imageAnnotations
+                imageAnnotations.append(annotation)
+
+        self.images =  [ image for image in annotations["images"] if image["id"] in self.annotations ]
 
         self.imagesDir = imagesDir
         self.masksDir = masksDir
-        self.indices = list(range(len(annotations["images"])))
+        self.indices = list(range(len(self.images)))
         if shuffle:
             random.shuffle(self.indices)
         self.index = 0
-    
-    def readBatch(self, batchSize: int):
-        batch = []
-        while self.index < len(self.indices) and len(batch) < batchSize:
+
+    def __len__(self):
+        return len(self.images)
+
+    def readBatch(self, batchSize: int = None, loadMaskImages: bool = False):
+        if batchSize is None:
+            batchSize = len(self.images)
+        imagesBatch = []
+        masksBatch = []
+        maksImagesBacth = []
+        while self.index < len(self.indices) and len(imagesBatch) < batchSize:
             datasetIndex = self.indices[self.index]
             imageInfo = self.images[datasetIndex]
             imageName = imageInfo["file_name"]
@@ -52,11 +62,24 @@ class CocoDataset:
                     commonMask = mask
                 else:
                     commonMask = np.logical_or(commonMask, mask)
-            commonMask = commonMask.astype(np.uint8)
+            commonMask = commonMask.astype(np.float32)
             maskName = os.path.splitext(imageName)[0] + ".png"
-            fileMask = cv2.imread(os.path.join(self.masksDir, maskName))
+            maskImage = cv2.imread(os.path.join(self.masksDir, maskName))
 
             if image is not None and commonMask is not None:
-                batch.append([image, commonMask, fileMask])
+                if image.shape[0] != 480 or image.shape[1] != 640:
+                    image = cv2.resize(image, (640, 480))
+                if commonMask.shape[0] != 480 or commonMask.shape[1] != 640:
+                    commonMask = cv2.resize(commonMask, (640, 480))
+
+        
+
+                imagesBatch.append(image)
+                masksBatch.append(commonMask)
+                if loadMaskImages:
+                    maksImagesBacth.append(maskImage)
             self.index += 1
-        return batch
+        imagesBatch = tf.convert_to_tensor(np.stack(imagesBatch))
+        masksBatch = tf.convert_to_tensor(np.stack(masksBatch))
+
+        return (imagesBatch, masksBatch, maksImagesBacth) if loadMaskImages else (imagesBatch, masksBatch)
--- broccole/train.py
+++ broccole/train.py
@@ -1,24 +1,55 @@
 import segmentation_models as sm
+import argparse
 import os
 import cv2
 
 from broccole.CocoDataset import CocoDataset
 
-def main():    
-    #TODO:
-    # load coco dataset: images, mask
-    # init model
-    # train
-    # iterate over dataset with batches
-    # 
-    datasetDir = 'D:\\data\\segmentation\\data\\coco'
-    cocoDataset = CocoDataset(
-        os.path.join(datasetDir, 'annotations/instances_train2017.json'), #'annotations/panoptic_train2017.json'),
-        os.path.join(datasetDir, 'train2017'),
-        os.path.join(datasetDir, 'annotations/panoptic_train2017')
+def train(trainDataset: CocoDataset, validationDataset: CocoDataset):
+    encoder = 'resnet18'
+    preprocess_input = sm.get_preprocessing(encoder)
+
+    model = sm.Unet(encoder, classes=1, encoder_weights='imagenet')
+    model.compile(
+        'Adam',
+        loss=sm.losses.bce_jaccard_loss,
+        metrics=[sm.metrics.iou_score],
     )
 
-    batch = cocoDataset.readBatch(10)
+    validationPacketSize = 32 * 32
+    x_val, y_val = validationDataset.readBatch(validationPacketSize)
+    x_val = preprocess_input(x_val)
+
+    packetSize = 16 * 16
+    batchSize = 1 # 16
+    epochs = 100
+    for epoch in range(epochs):
+        print('epoch {}'.format(epoch))
+        packets = len(trainDataset) // packetSize
+        for _ in range(packets - 1):
+            x_train, y_train = trainDataset.readBatch(packetSize)
+            x_train = preprocess_input(x_train)
+
+            model.fit(
+                x=x_train,
+                y=y_train,
+                batch_size=batchSize,
+                epochs=1,
+                validation_data=(x_val, y_val),
+            )
+
+        packet = trainDataset.readBatch(packetSize)
+
+        model.fit(
+            x=x_train,
+            y=y_train,
+            batch_size=16,
+            epochs=1,
+            validation_data=(x_val, y_val),
+        )
+
+def showDataset(dataset: CocoDataset):
+    batch = dataset.readBatch(10, loadMaskImages=True)
     for imageMask in batch:
         cv2.imshow('image', imageMask[0])
         cv2.imshow('mask', imageMask[1] * 255)
@@ -30,6 +61,39 @@ def main():
         cv2.waitKey(0)
 
     cv2.destroyAllWindows()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='train U-Net')
+    parser.add_argument('--datasetDir', help='path to directory with dataset', type=str)    
+    args = parser.parse_args()
+    return args
+
+def main():
+    #TODO:
+    # load coco dataset: images, mask
+    # init model
+    # train
+    # iterate over dataset with batches
+    # 
+
+    args = parse_args()
+    # datasetDir = 'D:\\data\\segmentation\\data\\coco'
+    datasetDir = args.datasetDir
+    trainDataset = CocoDataset(
+        os.path.join(datasetDir, 'annotations/instances_train2017.json'),
+        os.path.join(datasetDir, 'train2017'),
+        os.path.join(datasetDir, 'annotations/panoptic_train2017'),
+        classes=[1]
+    )
+
+    valDataset = CocoDataset(
+        os.path.join(datasetDir, 'annotations/instances_val2017.json'),
+        os.path.join(datasetDir, 'val2017'),
+        os.path.join(datasetDir, 'annotations/panoptic_val2017'),
+        classes=[1]
+    )
+
+    train(trainDataset, valDataset)
 
 
 if __name__ == '__main__':
