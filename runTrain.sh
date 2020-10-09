f22608ca59578d790160983e51afe73317eb7296
Kapulkin Stanislav <kapulkin@gmail.com>
2020-10-09 22:24:51 +0300

    [*] run  commands update

---
+++ runTrain.sh
@@ -0,0 +1,3 @@
+python -m broccole.trainPrepared --datasetDir E:\data\segmentation\data\coco --datasetType coco --batchSize 8 --epochs 100
+
+python -m broccole.prepareDataset --datasetDir E:\data\segmentation\data\coco
