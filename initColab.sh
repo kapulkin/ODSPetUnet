48084d08674b3a36cca6d405b4fcf960bcf51d0c
Kapulkin Stanislav <kapulkin@gmail.com>
2020-10-09 05:19:36 +0300

    [+] Colab scripts

---
+++ initColab.sh
@@ -0,0 +1,8 @@
+!git config user.name "Stanislav Kapulkin"
+!git config user.email "kapulkin@gmail.com"
+
+!git clone https://staskorg@bitbucket.org/broccole/unet.git
+
+%cd unet
+
+!pip install segmentation_models
---
+++ runColab.sh
@@ -0,0 +1,2 @@
+%env SM_FRAMEWORK=tf.keras
+!python -m broccole.trainPrepared --datasetDir '../drive/My Drive/broccole/data/' --batchSize 1 --epochs 100
